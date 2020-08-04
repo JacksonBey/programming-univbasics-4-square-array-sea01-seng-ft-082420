37e36e9bf1c4bf715040a662961700b5ca194ccd
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-04 03:52:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
-
+counter = 0
+strarr=[]
+  while array[counter] do
+    strarr << Math.sqrt(array[counter])
+    counter += 1
+  end
+  return strarr
 end
-
-
-
-
-ans=Math.sqrt(num)
